512a1d602e96ce343a952255a66e0322dec1e38e
zhuo-spring <793651012@qq.com>
2024-10-27 18:56:31 +0800

    fix:单词拼错

--- src/view/schedule/components/personChooseDialog.vue
+++ src/view/schedule/components/personChooseDialog.vue
@@ -73,7 +73,7 @@ export default {
     watch: {
         personType: {
             handler() {
-                this.handleRest();
+                this.handleReset();
             }
         }
     },
