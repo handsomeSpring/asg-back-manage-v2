17cd36ac700b926e96d8bc38edc9ea856a3c64c2
theres-rain <3239495270@qq.com>
2024-11-04 12:52:13 +0800

    feat:导播审核裁判审核违规名单登记

---
+++ src/view/directoremploy/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>2222</div>
+</template>
+
+<script>
+export default {
+  name:'test-1',
+    data() {
+      return {
+
+      };
+    },
+}
+</script>
+<style lang='less' scoped>
+</style>
