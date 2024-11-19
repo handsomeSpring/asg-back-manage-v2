bcf3175af79b0f55fa77e0028c2cfe56663bd809
fangyunong <fangyunong@teamlead.cn>
2024-11-19 17:04:19 +0800

    feat:赛季修改

---
+++ src/view/seasonManage/components/AsgProgress.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="status-grid-container">
+    <div class="grid-item" v-for="(item, index) in list" :key="index">
+      <div class="label">{{ item.label }}</div>
+      <div class="circle" :class="item.status < Number(status) ? '' : item.status === Number(status) ? 'process' : 'wait'">
+        <div class="inner" v-if="item.status === Number(status)"></div>
+        <i class="el-icon-check" v-if="item.status < Number(status)"></i>
+      </div>
+      <div class="line" :class="item.status < Number(status) ? '' : item.status === Number(status) ? 'process' : 'wait'"></div>
+    </div>
+    <div class="grid-item">
+      <div class="label">赛季结束</div>
+      <div class="circle" :class="status === '5' ? '' : 'wait'"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AsgProgress",
+  props: {
+    status: {
+      type: String,
+      default: "0",
+    },
+  },
+  data() {
+    return {
+      list: [
+        {
+          label: "赛季开始",
+          status: 0,
+        },
+        {
+          label: "报名中",
+          status: 1,
+        },
+        {
+          label: "公开报名中",
+          status: 2,
+        },
+        {
+          label: "表单修改期",
+          status: 3,
+        },
+        {
+          label: "比赛中",
+          status: 4,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped lang="less">
+.status-grid-container {
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  align-items: center;
+  width: 100%;
+  .grid-item {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    position: relative;
+    height: 40px;
+    .label {
+      position: absolute;
+      bottom: 40px;
+      left: 0;
+      font-size: 14px;
+      color: #333;
+      font-weight: 500;
+    }
+    .circle {
+      background: #64d13b;
+      width: 20px;
+      height: 20px;
+      border-radius: 50%;
+      flex-shrink: 0;
+      color: #fff;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      .inner{
+        width:10px;
+        height:10px;
+        background:#fff;
+        border-radius:50%;
+      }
+      &.process{
+        background: #4090ef;
+      }
+      &.wait{
+        background:#979797;
+      }
+    }
+    .line {
+      height: 3px;
+      background: #64d13b;
+      width: 100%;
+      margin: 0 12px;
+      &.process{
+        background: #4090ef;
+      }
+      &.wait{
+        background:#979797;
+      }
+    }
+  }
+}
+</style>
--- src/view/seasonManage/index.vue
+++ src/view/seasonManage/index.vue
@@ -1,57 +1,102 @@
 <template>
   <div>
     <header>
-      <el-button icon="el-icon-plus" size="small" type="primary" @click="openDialog('add')">开启新赛季</el-button>
+      <el-button
+        icon="el-icon-plus"
+        size="small"
+        type="primary"
+        @click="openDialog('add')"
+        >开启新赛季</el-button
+      >
     </header>
-    <el-scrollbar style="height: 70vh">
-      <baseTable v-loading="loading" :data="events" :column="tableProps">
-        <template #projectHeader="{ data }">
-          <div>
-            <span class="fontWeight">{{ data.name }}</span>
+    <el-scrollbar
+      v-if="events.length > 0"
+      style="height: 70vh"
+      v-loading="loading"
+    >
+      <asg-table-card v-for="(item, index) in events" :key="index">
+        <template #header>
+          <div class="asg-header">
+            <div class="header-left">
+              <span class="fontWeight">{{ item.name }}</span>
+              <span class="time-info">
+                {{ new Date(item.opentime) | parseTime("{y}-{m}-{d}") }}</span
+              >
+              <el-tag
+                size="small"
+                :type="item.is_over ? 'danger' : 'primary'"
+                >{{ item.is_over ? "已结束" : "进行中" }}</el-tag
+              >
+            </div>
+            <div class="header-operation">
+              <el-button type="text" @click="checkRuleInfo(item)">
+                预览规则
+              </el-button>
+              <el-button type="text" @click="handleUpdate(item)">
+                编辑赛季
+              </el-button>
+              <el-button
+                style="color: #f40"
+                type="text"
+                @click="deleteItem(item.id)"
+              >
+                删除赛季
+              </el-button>
+            </div>
           </div>
-          <div>
-            <span><i class="el-icon-edit" style="
-                  color: rgb(91, 182, 242);
-                  font-size: 16px;
-                  cursor: pointer;
-                " @click="handleUpdate(data)"></i></span>
-            <i class="el-icon-delete" style="color: red; font-size: 16px; cursor: pointer"
-              @click="deleteItem(data.id)"></i>
+        </template>
+        <template #content>
+          <div class="progress-content">
+            <AsgProgress :status="item.status"></AsgProgress>
           </div>
         </template>
-        <template #opentime="{ data }">
-          {{ new Date(data.opentime) | parseTime("{y}-{m}-{d}") }}
-        </template>
-        <template #over="{ data }">
-          <el-tag :type="data.is_over ? 'danger' : 'primary'">{{
-            data.is_over ? "已结束" : "进行中"
-          }}</el-tag>
-        </template>
-        <template #status="{ data }">
-          <span class="season_text">{{ data.status | filterStatus }}</span>
-        </template>
-        <template #rule="{ data }">
-          <el-button type="text" @click="checkRuleInfo(data)">
-            预览规则
-          </el-button>
-        </template>
-      </baseTable>
+      </asg-table-card>
     </el-scrollbar>
-    <el-dialog top="5vh" :close-on-click-modal="false" append-to-body :title="title" width="70%"
-      :visible.sync="dialogVisible">
-      <el-form ref="seasonForm" :model="seasonForm" label-width="120px" label-position="right" :rules="formRules">
+    <div class="no-message" v-else>暂无赛季</div>
+    <!-- 赛季详情 -->
+    <el-dialog
+      top="5vh"
+      :close-on-click-modal="false"
+      append-to-body
+      :title="title"
+      width="70%"
+      :visible.sync="dialogVisible"
+    >
+      <el-form
+        ref="seasonForm"
+        :model="seasonForm"
+        label-width="120px"
+        label-position="right"
+        :rules="formRules"
+      >
         <el-row type="flex">
           <el-col :span="11">
             <el-form-item label="赛季名" prop="name">
-              <el-input size="mini" v-model="seasonForm.name" placeholder="输入赛季名"></el-input>
+              <el-input
+                size="mini"
+                v-model="seasonForm.name"
+                placeholder="输入赛季名"
+              ></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="11" :offset="2">
             <el-form-item label="赛季开始时间" prop="opentime">
-              <el-date-picker v-if="type === 'add'" v-model="seasonForm.opentime" size="small" type="datetime"
-                placeholder="选择日期时间" value-format="yyyy-MM-ddThh:mm:ss.656Z" format="yyyy-MM-dd hh:mm:ss">
+              <el-date-picker
+                v-if="type === 'add'"
+                v-model="seasonForm.opentime"
+                size="small"
+                type="datetime"
+                placeholder="选择日期时间"
+                value-format="yyyy-MM-ddThh:mm:ss.656Z"
+                format="yyyy-MM-dd hh:mm:ss"
+              >
               </el-date-picker>
-              <el-input v-else v-model="seasonForm.opentime" size="small" placeholder="格式yyyy-MM-ddThh:mm:ss.656Z">
+              <el-input
+                v-else
+                v-model="seasonForm.opentime"
+                size="small"
+                placeholder="格式yyyy-MM-ddThh:mm:ss.656Z"
+              >
               </el-input>
             </el-form-item>
           </el-col>
@@ -81,20 +126,44 @@
         <template>
           <label><span style="color: #f40">*</span>赛季规则</label>
           <el-row style="margin-top: 12px">
-            <v-md-editor v-model="rules" height="400px" width="800"></v-md-editor>
+            <v-md-editor
+              v-model="rules"
+              height="400px"
+              width="800"
+            ></v-md-editor>
           </el-row>
         </template>
         <div style="text-align: right; margin-top: 12px">
-          <el-button v-if="type === 'add'" size="small" type="primary" @click="handleAddEvent">发布</el-button>
-          <el-button v-else type="primary" size="small" @click="updateItem">更新</el-button>
-          <el-button size="small" plain @click="dialogVisible = false">关 闭</el-button>
+          <el-button
+            v-if="type === 'add'"
+            size="small"
+            type="primary"
+            @click="handleAddEvent"
+            >发布</el-button
+          >
+          <el-button v-else type="primary" size="small" @click="updateItem"
+            >更新</el-button
+          >
+          <el-button size="small" plain @click="dialogVisible = false"
+            >关 闭</el-button
+          >
         </div>
       </el-form>
     </el-dialog>
-    <el-dialog top="5vh" title="规则详情" append-to-body width="60%"
-      :visible.sync="checkDialogVisible">
+    <!-- 赛季规则 -->
+    <el-dialog
+      top="5vh"
+      title="规则详情"
+      append-to-body
+      width="60%"
+      :visible.sync="checkDialogVisible"
+    >
       <el-scrollbar style="height: 55vh">
-        <v-md-preview :text="checkRules" height="400px" width="800"></v-md-preview>
+        <v-md-preview
+          :text="checkRules"
+          height="400px"
+          width="800"
+        ></v-md-preview>
       </el-scrollbar>
     </el-dialog>
   </div>
@@ -107,11 +176,13 @@ import {
   deleteEvents,
   updateEvents,
 } from "@/api/gameSeason/index";
-import baseTable from "@/components/baseTable.vue";
+import asgTableCard from "@/components/asg-table-card.vue";
+import AsgProgress from './components/AsgProgress.vue';
 export default {
   name: "seasonManage",
   components: {
-    baseTable,
+    asgTableCard,
+    AsgProgress
   },
   data() {
     return {
@@ -127,57 +198,7 @@ export default {
       dialogVisible: false, //新增/编辑窗口
       rules: "",
       title: "新增赛季",
-      tableProps: [
-        {
-          type: "index",
-          label: "序号",
-          props: "index",
-          style: {
-            textAlign: "center",
-            minWidth: "80px",
-          },
-        },
-        {
-          type: "opentime",
-          label: "开启时间",
-          prop: "opentime",
-          slot: true,
-          style: {
-            textAlign: "center",
-            width: "200px",
-          },
-        },
-        {
-          type: "over",
-          label: "是否结束",
-          prop: "over",
-          slot: true,
-          style: {
-            textAlign: "center",
-            minWidth: "100px",
-          },
-        },
-        {
-          type: "status",
-          label: "赛季执行阶段",
-          prop: "status",
-          slot: true,
-          style: {
-            textAlign: "center",
-            minWidth: "150px",
-          },
-        },
-        {
-          type: "rule",
-          label: "规则",
-          prop: "rule",
-          slot: true,
-          style: {
-            textAlign: "center",
-            minWidth: "120px",
-          },
-        },
-      ],
+
       checkRules: "",
       checkDialogVisible: false,
       formRules: {
@@ -187,19 +208,6 @@ export default {
         ],
       },
     };
-  },
-  filters: {
-    filterStatus(val) {
-      const mapList = {
-        0: "未开始",
-        1: "报名中",
-        2: "公开报名中",
-        3: "表单修改期",
-        4: "比赛中",
-        5: "赛季结束",
-      };
-      return mapList[val];
-    },
   },
   computed: {
     serveIp() {
@@ -264,15 +272,18 @@ export default {
             ...this.seasonForm,
             rule_markdown: this.rules,
           };
-          pushNewEvents(reqDTO).then(({ status }) => {
-            if (status !== 200) throw new Error('服务端异常，新增赛季失败！');
-            this.$message.success('新增赛季成功！');
-            this.initEvents();
-          }).catch(error => {
-            this.$message.error(error.message);
-          }).finally(() => {
-            this.dialogVisible = false;
-          })
+          pushNewEvents(reqDTO)
+            .then(({ status }) => {
+              if (status !== 200) throw new Error("服务端异常，新增赛季失败！");
+              this.$message.success("新增赛季成功！");
+              this.initEvents();
+            })
+            .catch((error) => {
+              this.$message.error(error.message);
+            })
+            .finally(() => {
+              this.dialogVisible = false;
+            });
         }
       });
     },
@@ -280,7 +291,7 @@ export default {
       try {
         this.loading = true;
         const { data, status } = await getAllEvents();
-        if (status !== 200) throw new Error('服务端异常，获取赛季失败！');
+        if (status !== 200) throw new Error("服务端异常，获取赛季失败！");
         this.events = data;
       } catch (error) {
         this.$message.error(error.message);
@@ -307,14 +318,14 @@ export default {
               this.$message.error("服务器异常，请联系管理员！");
             });
         })
-        .catch(() => { });
+        .catch(() => {});
     },
     async updateItem() {
       try {
         const req = {
           ...this.seasonForm,
           rule_markdown: this.rules,
-        }
+        };
         const { status } = await updateEvents(req);
         if (status !== 200) throw new Error("服务端异常，请联系网站管理员！");
         this.dialogVisible = false;
@@ -332,15 +343,55 @@ export default {
 header {
   margin: 0 0 12px;
 }
+.asg-header {
+  height: inherit;
+  padding: 0 18px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .header-left {
+    display: flex;
+    align-items: center;
+    gap:12px;
+    .fontWeight {
+      font-weight: 600;
+      color: #4f73eb;
+    }
+    .time-info{
+      font-size: 14px;
+      color:#333333;
+      font-weight: 400;
+    }
+  }
 
-.fontWeight {
-  font-weight: bold;
-  color: #4f73eb;
+  // 操作按钮
+  .header-operation {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 18px;
+  }
 }
-.season_text{
-  color: rgb(161, 175, 241);
-  text-decoration: underline;
-  font-weight: bold;
-  text-underline-offset: 0.2rem;
+.progress-content{
+  padding:24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+// .season_text {
+//   color: rgb(161, 175, 241);
+//   text-decoration: underline;
+//   font-weight: bold;
+//   text-underline-offset: 0.2rem;
+// }
+.no-message {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100px;
+  width: 100%;
+  color: #979797;
+  border-top: 1px solid #979797;
 }
 </style>
